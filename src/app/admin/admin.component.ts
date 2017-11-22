90311c90f57c654cf5122653505505ada3ce806a
omegadeep10 <omegadeep10@outlook.com>
2017-11-22 10:35:25 -0500

    Add faculty progress year filter

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -8,14 +8,16 @@ import { SLOService, AlertService, FacultyService } from './../_services/';
   styleUrls: ['./admin.component.sass']
 })
 export class AdminComponent implements OnInit {
-  reportLoading: Boolean = false;
-  loadingProgress: Boolean = false;
-  file_url: String = "";
-  courses: Course[];
-  uniqueFaculty: Faculty[] = [];
-  currentSection: String = 'Class Reports'
-  uniqueYears = null;
-  
+  reportLoading: Boolean = false; //loading state variable for reports section
+  loadingProgress: Boolean = false; //loading state variable for progress section
+  file_url: String = ""; // holds download link when report is successfully generated
+  courses: Course[]; //list of all courses from the progress endpoint.
+  uniqueYears: Set<Date> = new Set();
+  uniqueFaculty: Faculty[] = []; //holds unique faculty members
+  currentSection: String = 'Class Reports';
+  faculty_progress_year; // holds selection for faculty progress dropdown
+
+  // form data model for the export selection
   export_form = {
     course_type: 'courses',
     year: 'All Years'
@@ -34,10 +36,10 @@ export class AdminComponent implements OnInit {
         this.courses = data;
 
         //used to populate year dropdown
-        this.uniqueYears = new Set(data.map(x => x.course_year));
-
+        this.uniqueYears = new Set(data.map(x => x.course_year).sort().reverse());
+        this.faculty_progress_year = Array.from(this.uniqueYears)[0];
         // basically populate uniqueFaculty with a list of faculty members WITHOUT duplicates
-        for (let course of this.courses) {
+        for (let course of this.courses.filter(x => x.course_year == this.faculty_progress_year)) {
           if (this.uniqueFaculty.map(e => e.faculty_id).indexOf(course.faculty.faculty_id) == -1) {
             this.uniqueFaculty.push(course.faculty);
           }
@@ -52,8 +54,19 @@ export class AdminComponent implements OnInit {
     )
   }
 
+  updateFacultyProgress() {
+    this.uniqueFaculty = []; //empty out uniqueFaculty
+
+    //regenerate uniqueFaculty based on newly selected course_year
+    for (let course of this.courses.filter(x => x.course_year == this.faculty_progress_year)) {
+      if (this.uniqueFaculty.map(e => e.faculty_id).indexOf(course.faculty.faculty_id) == -1) {
+        this.uniqueFaculty.push(course.faculty);
+      }
+    }
+  }
+
   getFacultyCourses(faculty) {
-    return this.courses.filter(x => x.faculty.faculty_id == faculty.faculty_id);
+    return this.courses.filter(x => x.faculty.faculty_id == faculty.faculty_id && x.course_year == this.faculty_progress_year);
   }
 
   generateReport() {
